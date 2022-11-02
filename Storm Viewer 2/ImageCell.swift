f5a6c0a45b8dd352275888109f11da7ab571b465
Melis Yazıcı <melis.yazici95@icloud.com>
2022-11-02 23:37:33 +0100

    Design UICollectionView cells

---
+++ Storm Viewer 2/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCell.swift
+//  Storm Viewer 2
+//
+//  Created by Melis Yazıcı on 02.11.22.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var imageLabel: UILabel!
+}
