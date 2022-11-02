149d2c0476f036f3c40f2a3e8a5a563b2a748df3
Melis Yazıcı <melis.yazici95@icloud.com>
2022-11-02 21:36:21 +0100

    Initial Commit

---
+++ Storm Viewer 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storm Viewer 2
+//
+//  Created by Melis Yazıcı on 02.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
