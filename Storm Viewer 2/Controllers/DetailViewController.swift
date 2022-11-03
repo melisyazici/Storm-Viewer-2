4e81af7bcd4d68dba1e1b221eed23b2d4c9e4196
Melis Yazıcı <melis.yazici95@icloud.com>
2022-11-03 01:57:54 +0100

    Transfer images to DetailViewController

--- Storm Viewer 2/Controllers/DetailViewController.swift
+++ Storm Viewer 2/Controllers/DetailViewController.swift
@@ -17,18 +17,11 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
